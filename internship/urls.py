bee78e41a0aa47a58ec5b7ecdd39a71f4f9fd72b
Samuel Ogboye <ogboyesam@gmail.com>
2024-06-21 23:16:47 +0100

    fix(url): added trailing path

--- internship/urls.py
+++ internship/urls.py
@@ -7,8 +7,8 @@ from .views import (
 )
 
 urlpatterns = [
-    path('register/entry', InternshipApplicationView.as_view(), name='internship-application'),
-    path('applications', InternshipApplicationListView.as_view(), name='internship-application-list'),
+    path('register/entry/', InternshipApplicationView.as_view(), name='internship-application'),
+    path('applications/', InternshipApplicationListView.as_view(), name='internship-application-list'),
     path('applications/<uuid:pk>', InternshipApplicationDetailView.as_view(), name='internship-application-detail'),
     path('applications/delete/<uuid:pk>', InternshipApplicationDeleteView.as_view(), name='internship-application-delete'),
 ]
