4b9a1f2774b5996d59caef57efbcd3fd79663d14
Samuel Ogboye <ogboyesam@gmail.com>
2025-03-19 01:25:20 +0100

    fix: update model attribute for link

--- internship/models.py
+++ internship/models.py
@@ -6,14 +6,14 @@ class InternshipApplication(BaseModel):
     email = models.EmailField(max_length=255)
     full_name = models.CharField(max_length=255)
     twitter_handle = models.CharField(max_length=255)
-    linkedin = models.URLField(max_length=255)
+    linkedin = models.CharField(max_length=255)
     skill = models.CharField(max_length=255)
     experience = models.TextField()
-    github_link= models.URLField(max_length=255, default="")
+    github_link= models.CharField(max_length=255, default="")
     about_skill = models.TextField()
     certificate = models.FileField(upload_to='certificates/', max_length=500, blank=True, null=True)
     commitment = models.BooleanField()
-    birthdate = models.DateField()
+    birthdate = models.DateField(blank=True, null=True)
 
 
     def __str__(self):
