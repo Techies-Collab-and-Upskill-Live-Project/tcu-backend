d3e2517c578387685fe966073a1547c7517f2050
Samuel Ogboye <ogboyesam@gmail.com>
2025-03-20 19:50:10 +0100

    fix: ensure access to download logs is authenticated

--- core/views.py
+++ core/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import redirect
 import os
 from django.conf import settings
 from django.http import HttpResponse, Http404
-
+from django.contrib.auth.decorators import login_required
 
 def return_home_to_docs(request):
     return redirect("swagger-schema")
 
+@login_required
 def download_logs(request):
     file_path = os.path.join(settings.BASE_DIR, 'general.log')
-    print("file", file_path)
     if os.path.exists(file_path):
         with open(file_path, 'rb') as file:
             response = HttpResponse(file.read(), content_type='text/plain')
